14de9797a7a8283128ff26326ad2d85ab31b948d
Weway-git <1419864987@qq.com>
2020-02-28 01:02:51 +0800

    2

--- src/chapter6/class_var.py
+++ src/chapter6/class_var.py
@@ -1,7 +1,25 @@
-class Address:
-    detail = '广州'
-    post_code = '50000'
-    def info(self):
-        # print(detail)
-        print(Address.detail)
+# class Address:
+#     detail = '广州'
+#     post_code = '50000'
+#     def info(self):
+#         # print(detail)
+#         print(Address.detail)
+#         print(Address.post_code)
 
+
+# print(Address.detail)
+# addr = Address()
+# addr.info()
+class Record:
+    item = "鼠标"
+    data = "2016-06-16"
+    def info(self):
+        print("info方法中：", self.item)
+        print("info方法中：", self.data)
+        print(self)
+
+
+rc=Record()
+print(rc.item)
+print(rc.data)
+rc.info()
