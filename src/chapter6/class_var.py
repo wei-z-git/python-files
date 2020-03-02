260bece2faf514a975a93a63bf5ae59480affed1
Weway-git <1419864987@qq.com>
2020-03-02 09:10:09 +0800

    property

--- src/chapter6/class_var.py
+++ src/chapter6/class_var.py
@@ -10,16 +10,32 @@
 # print(Address.detail)
 # addr = Address()
 # addr.info()
-class Record:
-    item = "鼠标"
-    data = "2016-06-16"
-    def info(self):
-        print("info方法中：", self.item)
-        print("info方法中：", self.data)
-        print(self)
+# class Record:
+#     item = "鼠标"
+#     data = "2016-06-16"
+#     def info(self):
+#         print("info方法中：", self.item)
+#         print("info方法中：", self.data)
+#         print(self)
 
 
-rc=Record()
-print(rc.item)
-print(rc.data)
-rc.info()
+# rc=Record()
+# print(rc.item)
+# print(rc.data)
+# rc.info()
+
+class Inventory:
+    item = '鼠标'
+    quantity = 2000
+    def change(self,item, quantity):
+        self.item = item
+        self.quantity = quantity
+    
+
+iv = Inventory()
+iv.change('haha',1000)
+print(iv.item)
+print(iv.quantity)
+
+print(Inventory.item)
+print(Inventory.quantity)
