110404e3c0154edc644681ad56d54a19cf027139
Weway-git <1419864987@qq.com>
2020-03-13 18:20:06 +0800

    zhuangshiqi

---
+++ src/装饰器/装饰器.py
@@ -0,0 +1,36 @@
+# import time
+
+
+# def index():
+#     time.sleep(3)
+#     print("welcome to index")
+
+
+# def timmer(func):
+#     start = time.time()
+#     func()
+#     stop = time.time()
+#     print("run time is %s" % (stop-start))
+
+# timmer(index)
+
+import time
+
+def timmer(func):
+    # func = index
+    def inner():
+        start = time.time()
+        func()
+        stop = time.time()
+        print("run time is %s" % (stop-start))
+    return inner
+
+
+@timmer
+def index():
+    time.sleep(3)
+    print("welcome to index")
+
+
+
+index()
