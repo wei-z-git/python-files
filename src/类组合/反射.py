5029e16f2a611ded3d4e54c3641e29d3c50074bf
Weway-git <1419864987@qq.com>
2020-03-29 19:52:18 +0800

    n

--- src/类组合/反射.py
+++ src/类组合/反射.py
@@ -7,17 +7,21 @@ class FtpClient:
     def interactie(self):
         while True:
             cmd = input('>>:').strip()
-            if not cmd:
+            if not cmd:  # if not (cmd is none)
                 continue
-            cmd_l=cmd.split()
+            cmd_l = cmd.split()
             print(cmd_l)
-            if hasattr(self,cmd_l)
+            if hasattr(self, cmd_l[0]):  # 判断对象是否有cmd_l的属性
+                func = getattr(self, cmd_l[0])
+                print(func)
+                func()
 
     def get(self):
         print('getting...')
-                
+
     def put(self):
         print('putting...')
+
 
 i = FtpClient('1.1.1.1', 3306)
 i.interactie()
