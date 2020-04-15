17d6b70cb16b2dbde447c40733d03ca1c725dc0e
Weway-git <1419864987@qq.com>
2020-04-15 23:04:27 +0800

    nianbao

--- src/粘包/客户端.py
+++ src/粘包/客户端.py
@@ -1,10 +1,24 @@
-from socket import AF_INET, SOCK_STREAM, socket
+import socket
+import struct
 
-client = socket(AF_INET, SOCK_STREAM)
-client.connect(('127.0.0.1', 8090))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 8080))
+while True:
 
-client.send('hello'.encode('utf-8'))
-client.send('world'.encode('utf-8'))
+    # 发收消息
+    cmd = input('>>>>:')
+    if not cmd:
+        continue
+    client.send(cmd.encode('utf-8'))
+
+    headers = client.recv(4)
+    total_size = struct.unpack('i', headers)
+    print(total_size)
+    # print('发送了：%s' % cmd)
+    # 收消息
+    # data = client.recv(1024)
+    # print('收到了：%s' % data)
 
 
+# 挂电话连接
 client.close()
--- src/粘包/服务端.py
+++ src/粘包/服务端.py
@@ -1,14 +1,33 @@
-from socket import AF_INET, SOCK_STREAM, socket
+import socket
+import subprocess
+import struct
 
-server = socket(AF_INET, SOCK_STREAM)
-server.bind(('127.0.0.1', 8090))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('127.0.0.1', 8080))
 server.listen(5)
+print('starting...')
 
-conn, client_addr = server.accept()
-print(client_addr)
-res1 = conn.recv(10)
-print('res1:', res1)
-res2 = conn.recv(10)
-print('res2:', res2)
-conn.close()
+while True:
+
+    conn, client_addr = server.accept()  # (conn,client_addr)
+    print(client_addr)
+
+    # 收发消息
+    while True:
+        try:
+            cmd = conn.recv(8096)
+            if not cmd:
+                break  # 针对linux，防止死循环
+            # 制作固定长度报头
+            stdout=obj.stdout.read()
+            stderr=obj.stderr.read()
+            total_size = len(stdout)+len(stdout)
+            headers = struct.pack('i', total_size)
+            print('收到客户端消息：%s' % cmd)
+            conn.send(cmd.upper())
+        except ConnectionResetError:
+            break
+    conn.close()
+
+# 关机
 server.close()
