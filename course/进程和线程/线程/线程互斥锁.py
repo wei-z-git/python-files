394040bce1eaf2fd156547012084a729201a0476
Weway-git <1419864987@qq.com>
2020-12-19 12:02:29 +0800

    线程互斥锁

--- course/进程和线程/线程/线程互斥锁.py
+++ course/进程和线程/线程/线程互斥锁.py
@@ -1,17 +1,24 @@
-from threading import Thread
-n=100
+from threading import Thread,Lock
 import time
+
+n=100
 def task():
     global n
-    temp=n
-    time.sleep(0.1)
-    n=temp-1 
+    with mutex: #相当于mutex.acquire(),运行完后自动mutex.release()
+        temp=n
+        time.sleep(0.1)
+        n=temp-1 
 
 if __name__ == "__main__":
+    start_time=time.time()
+    mutex=Lock()
     t_l=[]
     for i in range(100):
         t=Thread(target=task)
+        t_l.append(t)
         t.start()
     for t in t_l:
         t.join()
-    print('主')
+    stop_time=time.time()
+    print('主',n)
+    print('run time is %s'%(stop_time-start_time))
