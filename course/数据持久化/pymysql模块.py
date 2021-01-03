0e53ac4365da16a27e9f42dc67a81d8bed44e0bf
Weway-git <1419864987@qq.com>
2021-01-03 21:50:07 +0800

    pysql模块

--- course/数据持久化/pymysql模块.py
+++ course/数据持久化/pymysql模块.py
@@ -1,8 +1,19 @@
 import pymysql
 
-pymysql.connect(
-    host='192.168.1.106'
-    port=3306
-    user='root'
-    password='qiang666'
+client = pymysql.connect(
+    host='192.168.1.106',
+    port=3306,
+    user='root',
+    password='qiang666',
+    database='db6',
+    charset='utf8'
 )
+# 游标
+cursor = client.cursor()
+
+sql='insert into t1 values(1,"egon");'
+cursor.execute(sql)
+
+client.commit()
+cursor.close()
+client.close()
