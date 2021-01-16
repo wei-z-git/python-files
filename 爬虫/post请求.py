4d8410b8fa7a7789ef17bf978486a9e46d5736c6
Weway-git <1419864987@qq.com>
2021-01-16 22:35:56 +0800

    POST请求

--- 爬虫/post请求.py
+++ 爬虫/post请求.py
@@ -1,11 +1,30 @@
 import requests
 import re
-res = requests.get(url='https://github.com/login',
-                   headers={
-                       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3314.0 Safari/537.36 SE 2.X MetaSr 1.0',
-                   },
-                   )
+r1 = requests.get(url='https://github.com/login',
+                  headers={
+                      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3314.0 Safari/537.36 SE 2.X MetaSr 1.0',
+                  },
+                  )
 
 authenticity_token = re.findall(
-    'name="authenticity_token".*?value="(.*?)"', res.text, re.S)[0]
-# requests.post(url='xx',)
+    'name="authenticity_token".*?value="(.*?)"', r1.text, re.S)[0]
+r1_cookies = r1.cookies.get_dict()
+print(r1_cookies)
+r2 = requests.post(url='https://github.com/session',
+                   headers={
+                       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3314.0 Safari/537.36 SE 2.X MetaSr 1.0',
+                       'Referer': 'https://github.com/login',
+                   },
+                   data={
+
+                       'commit': 'Sign in',
+                       'authenticity_token': authenticity_token,
+                       'login': '1419864987@qq.com',
+                       'password': 'wodeyuwen666',
+                   },
+                   cookies=r1_cookies,
+
+                   )
+print(r2.status_code)
+
+requests.get()
