387548ad1f1e16852b239c4c0ffa8f8c602981e4
Weway-git <1419864987@qq.com>
2021-02-01 22:35:52 +0800

    回顾同步调用

---
+++ 爬虫/性能/同步调用.py
@@ -0,0 +1,30 @@
+import requests
+from threading import Thread, current_thread
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
+
+def parse_page(res):
+    res = res.result()
+    print('%s PARSE %s' % (current_thread().getName(), len(res)))
+
+
+def get_page(url, callback=parse_page):
+    print('%s GET %s' % (current_thread().getName(), url))
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+
+
+if __name__ == '__main__':
+    urls = [
+        'https://www.baidu.com',
+        'https://www.taobao.com',
+        'https://www.hao123.com'
+
+    ]
+
+    pool = ThreadPoolExecutor(50)
+    for url in urls:
+        pool.submit(get_page, url).add_done_callback(parse_page)
+
+    pool.shutdown(wait=True)
