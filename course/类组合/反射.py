a416c400c14cea3ccb8d9969f0a0ece52a0ae96c
Weway-git <1419864987@qq.com>
2021-03-08 21:18:03 +0800

    反射复习

--- course/类组合/反射.py
+++ course/类组合/反射.py
@@ -13,7 +13,7 @@ class FtpClient:
             cmd_list = cmd.split()
             print(cmd_list)
             if hasattr(self, cmd_list[0]):  # 判断对象是否有cmd_list的属性
-                func = getattr(self, cmd_list[0])
+                func = getattr(self, cmd_list[0]) #判断是否有属性
                 print(func)
                 func()
 
@@ -26,3 +26,8 @@ class FtpClient:
 
 i = FtpClient('1.1.1.1', 3306)
 i.interactie()
+
+""" 
+反射的目的：
+应该就是为了在函数内部判断时，不用写一堆判断
+ """
